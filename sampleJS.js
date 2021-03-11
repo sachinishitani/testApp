f4d93b0d8d720803b6a0fab50788d152b05144d2
sachinishitani <ichi5sachi@gmail.com>
2021-03-11 16:01:56 +0900

    Add:Time

--- sampleJS.js
+++ sampleJS.js
@@ -3,3 +3,19 @@ function altRan2() {
 
   document.getElementById("sai").innerHTML = r; //値の出力
 }
+
+document.getElementById("view_time").innerHTML = getNow();
+
+function getNow() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var mon = now.getMonth()+1; //１を足すこと
+	var day = now.getDate();
+	var hour = now.getHours();
+	var min = now.getMinutes();
+	var sec = now.getSeconds();
+
+	//出力用
+	var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒"; 
+	return s;
+}
